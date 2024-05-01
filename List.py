ed78686e2c374c020a3443ab20480a8f38f64f8d
Rifat <rifatyou2686@gmail.com>
2024-05-01 23:13:46 +0600

    first commit

---
+++ List.py
@@ -0,0 +1,21 @@
+# r w  >
+list_a = [10, 20, 30, 40, 50, 60]
+a, b, c, *d = list_a
+print(a, b, c)
+print(d)
+# annoynomus Variable
+_, _, m, n, o, *p = list_a
+print(m, n, o)
+_,_,_,a,b,c,* d =list_a;
+print(a,b,c);
+Tuple1=(
+    "Bangladesh",
+    "China",
+    "India",
+    "Japan"
+)
+
+print('The size of Tuple is',len(Tuple1));
+for i in Tuple1:print(i);
+print(f'Index of {Tuple1} and the index of 0 is',Tuple1[0]);
+
